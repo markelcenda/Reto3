ffd780029ac8fa841bf0fcd80ffd581be2d8aeb2
markelcenda <markelcenda@gmail.com>
2020-11-11 14:29:44 +0100

    controlador

---
+++ controller/cUserById.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once '../model/usuarioModel.php';
+include_once '../model/equipoModel.php';
+
+$data=json_decode(file_get_contents("php://input"),true);
+
+$id=$data['id'];
+
+$usuario= new usuarioModel();
+$equipo=new equipoModel();
+
+$usuario->id=$id;
+
+$response=array();
+
+$response['jugador']=$usuario->findJugadorById();
+$response['entrenador']=$usuario->findEntrenadoreById();
+$response['delegado']=$usuario->findDelegadoById();
+$response['equipos']=$equipo->setList();
+
+$response['error']="no error";
+
+echo json_encode($response); // pasar de php a json
+
+unset ($usuario);
