34a7516dc01712c3fa1509e54ac093fb7648fd79
Galder <galdergomez50@gmail.com>
2020-11-11 10:13:09 +0100

    Seccion sobre nosotros añadida

--- view/js/jsFunctions.js
+++ view/js/jsFunctions.js
@@ -1,2 +1,11 @@
-
-
+$(window).on('load', function () {
+    aos_init();
+  });
+  
+  function aos_init() {
+      console.log("on");
+    AOS.init({
+      duration: 800,
+      once: true
+    });
+  }  
