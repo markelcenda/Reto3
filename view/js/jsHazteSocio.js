94ee11a5d967443aef586f51ed82e7446431cc11
Xabi <xaguirrearrien@gmail.com>
2020-11-12 14:07:34 +0100

    sesionDesdeCuaquierPagina

--- view/js/jsHazteSocio.js
+++ view/js/jsHazteSocio.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     
     $("#btnEnviar").click(datosFormulario);
     $("#imagen").change(changeImg);
+    $(".botonLoginStart").click(login);
+    $(".botonLogout").click(logout);
 
 });
 
@@ -153,5 +155,90 @@ function changeImg(){
             
         }
         
+}
+
+function login(){
+
+    //Variables que adquieren el valor de los datos introducidos en el modal
+	 usuario=$("#usuario").val();
+     password=$("#password").val();
+
+     //Filtro que mira si los campos usuario y contraseña estan vacio
+    if(usuario != "" && password != ""){
+    
+    //en caso de no estar vacio se mandaran los datos al controlador cLogin
+    var url = "../../controller/cLogin.php";
+    var data = {'usuario':usuario, 'password':password};
+
+    console.log(data);
+
+    		//Llamada fetch
+    		fetch(url, {
+    			  method: 'POST', 
+    			  body: JSON.stringify(data), // data can be `string` or {object}!
+    			  headers:{'Content-Type': 'application/json'}  // input data
+                  })
+                  
+    		.then(res => res.json()).then(result => {
+
+                alert(result.error); 
+
+                console.log(result.error);
+
+                //Filtro que mira si se ha podido inicar sesion
+                if(result.confirm == true){
+
+                    //Al detectar que si, econde el boton de iniciar sesion y muestra el de cerrarla y la imagen de usuario
+                    $(".botonLogin").hide();
+                    $(".botonLogout").show();
+                    $(".sesionUsuario").css('display','flex');
+                    $("#usuario").val("");
+                    $("#password").val("");
+                    $("#modelId").modal("hide");
+
+                    //Muestra la imagen que le corresponde al usuario que ha iniciado sesion
+                   img="<a href='#' class='ml-3' ><img id='imgSesion' src='../../view/uploads/" + result.usuarioSesion.imagen + "'></a>";
+
+                   $("#sitioUsuario").html(img);
+
+                }
+                
+    		})
+            .catch(error => console.error('Error status:', error));	
+
+        }else{
+
+            //en caso de estar vacios, aparecera un alert advirtiendo al usuario
+            alert("Los campos usuario y contraseña no pueden estar vacios");
+
+        }
+}
+
+//Fucnion de logout
+function logout(){
+
+    //Vacia los valores de los campos usuario y contraseña
+    $("#usuario").val("");
+    $("#password").val("");
+
+    //Esconde el boton de iniciar sesion y muestra el de cerrarla y el div que contiene la imagen del usuario
+    $(".botonLogin").show();
+    $(".botonLogout").hide();
+    $(".sesionUsuario").hide();
+
+    //Llamada fetch al controlador cLogout
+    var url = "../../controller/cLogout.php";
+	fetch(url, {
+		  method: 'GET', 
+		  headers:{'Content-Type': 'application/json'}
+		  })
+	.then(res => res.json()).then(result => {
+	
+        console.log(result.confirm);
+        alert(result.confirm);
+	
+	})
+	.catch(error => console.error('Error status:', error));	
+
 }
 
