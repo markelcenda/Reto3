6fdceb1b64498edaf307e8393c8495ebf48617a5
markelcenda <markelcenda@gmail.com>
2020-11-13 12:22:03 +0100

    cambios

--- view/js/jsFunctions.js
+++ view/js/jsFunctions.js
@@ -1,20 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   sessionVarsView();
   cargarImagenesEquipos();
-
-});
-
-$(window).on('load', function () {
   aos_init();
+
 });
 
-function aos_init() {
-  AOS.init({
-    duration: 800,
-    once: true
-  });
-<<<<<<< HEAD
-  
   function aos_init() {
     AOS.init({
       duration: 800,
@@ -56,8 +46,7 @@ function aos_init() {
     		})
     		.catch(error => console.error('Error status:', error));	
     }
-=======
-}
+
 
 function equiposDesdeIndex(id) {
   pagina = "view/pages/equipos.html?" + id;
@@ -148,4 +137,3 @@ function cargarImagenesEquipos() {
     })
     .catch(error => console.error('Error status:', error));
 }
->>>>>>> 4b775347cc57038bf5b30ffeffa14332b8331402
