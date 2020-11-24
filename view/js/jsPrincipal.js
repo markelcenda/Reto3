466af992e8c811257eefdcf41f672100dd205acc
Galder <galdergomez50@gmail.com>
2020-11-24 10:22:13 +0100

    modo oscuro en usuarios y socios

--- view/js/jsPrincipal.js
+++ view/js/jsPrincipal.js
@@ -1,47 +1,57 @@
 $(document).ready(function () {
-    nightModeOnLoad();
+  nightModeOnLoad();
 
-    $("#nightMode").click(nightMode);
+  $("#nightMode").click(nightMode);
 });
-
-/*Al pulsar enter evitamos hacer click en Inicar Sesion*/
-function enterSinClick(e){
-    if (e.keyCode === 13) {
-        e.preventDefault();
-        $(".botonLoginStart").click();
-       }
-}
 
 function nightMode() {
   if (localStorage.getItem('nightMode') != "true") {
     localStorage.setItem('nightMode', true);
-    $('#nightMode i').removeClass();
-    $('#nightMode i').addClass("fas");
-    $('#nightMode i').addClass("fa-sun");
-    $('body').removeClass('bg-light');
-    $('body').addClass('bg-dark');
+    setNightMode();
   } else {
     localStorage.setItem('nightMode', false);
-    $('#nightMode i').removeClass();
-    $('#nightMode i').addClass("fas");
-    $('#nightMode i').addClass("fa-moon");
-    $('body').removeClass('bg-dark');
-    $('body').addClass('bg-light');
+    unsetNightMode();
   }
 }
 
-function nightModeOnLoad(){
+function nightModeOnLoad() {
   if (localStorage.getItem('nightMode') != "true") {
-    $('#nightMode i').removeClass();
-    $('#nightMode i').addClass("fas");
-    $('#nightMode i').addClass("fa-moon");
-    $('body').removeClass('bg-dark');
-    $('body').addClass('bg-light');
+    unsetNightMode();
   } else {
-    $('#nightMode i').removeClass();
-    $('#nightMode i').addClass("fas");
-    $('#nightMode i').addClass("fa-sun");
-    $('body').removeClass('bg-light');
-    $('body').addClass('bg-dark');
+    setNightMode();
+  }
+}
+
+function setNightMode() {
+  $('#nightMode i').removeClass();
+  $('#nightMode i').addClass("fas fa-sun");
+  $('body').removeClass('bg-light');
+  $('body').addClass('bg-dark');
+  $('#noticias').removeClass('text-dark');
+  $("#noticias").addClass('text-white');
+  $('#insertCategoriaEquipo, #delanterosTitulo, #zaguerosTitulo, #liberosTitulo, #cuerpoTecnicoTitulo').removeClass('text-dark');
+  $('#insertCategoriaEquipo, #delanterosTitulo, #zaguerosTitulo, #liberosTitulo, #cuerpoTecnicoTitulo').addClass('text-white');
+  $('.textoModoNoche').removeClass('text-dark');
+  $('.textoModoNoche').addClass('text-white');
+}
+
+function unsetNightMode() {
+  $('#nightMode i').removeClass();
+  $('#nightMode i').addClass("fas fa-moon");
+  $('body').removeClass('bg-dark');
+  $('body').addClass('bg-light');
+  $('#noticias').removeClass('text-white');
+  $("#noticias").addClass('text-dark');
+  $('#insertCategoriaEquipo, #delanterosTitulo, #zaguerosTitulo, #liberosTitulo, #cuerpoTecnicoTitulo').removeClass('text-white');
+  $('#insertCategoriaEquipo, #delanterosTitulo, #zaguerosTitulo, #liberosTitulo, #cuerpoTecnicoTitulo').addClass('text-dark');
+  $('.textoModoNoche').removeClass('text-white');
+  $('.textoModoNoche').addClass('text-dark');
+}
+
+/*Al pulsar enter evitamos hacer click en Inicar Sesion*/
+function enterSinClick(e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    $(".botonLoginStart").click();
   }
 }
