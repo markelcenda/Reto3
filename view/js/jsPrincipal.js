b00704b49ed12d15c6bbf55fe5fe1c418864c66d
Galder <galdergomez50@gmail.com>
2020-11-26 11:48:53 +0100

    nuevo login

--- view/js/jsPrincipal.js
+++ view/js/jsPrincipal.js
@@ -5,7 +5,14 @@ $(document).ready(function () {
 
   menuDesplegable();
 
+  loadModal();
+
+  $('#loginSubmit').click(function(event){
+    event.preventDefault();
+  })
+
 });
+
 //Activa o desactiva el modo noche
 function nightMode() {
   if (localStorage.getItem('nightMode') != "true") {
@@ -16,6 +23,7 @@ function nightMode() {
     unsetNightMode();
   }
 }
+
 //Mira si la opcion de modo noche era false o true 
 function nightModeOnLoad() {
   if (localStorage.getItem('nightMode') != "true") {
@@ -24,6 +32,7 @@ function nightModeOnLoad() {
     setNightMode();
   }
 }
+
 //Pone la pagina en modo noche
 function setNightMode() {
   $('#nightMode i').removeClass();
@@ -37,6 +46,7 @@ function setNightMode() {
   $('.textoModoNoche').removeClass('text-dark');
   $('.textoModoNoche').addClass('text-white');
 }
+
 //Devuelve la pagina a su estilo original
 function unsetNightMode() {
   $('#nightMode i').removeClass();
@@ -67,5 +77,28 @@ function menuDesplegable(){
     e.stopPropagation();
     e.preventDefault();
 });
+}
 
+/*carga el modal en el div .modal-content*/
+function loadModal(){
+let modal = `<div class="login-form">
+              <form>
+                <div class="form">
+                  <h2 class="text-center">Iniciar Sesión</h2>
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>       
+                  <div class="form-group">
+                    <i class="fas fa-user" id="userIcon"></i>
+                    <input id="usuario" type="text" class="form-control" placeholder="Usuario" onkeyup="enterSinClick(event)">
+                  </div>
+                  <div class="form-group">
+                    <i class="fas fa-lock" id="userPadlock"></i>
+                    <input id="password" type="password" autocomplete="off" class="form-control" placeholder="Contraseña" onkeyup="enterSinClick(event)">
+                  </div>
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block botonLoginStart" id="loginSubmit">Iniciar Sesión</button>
+                  </div> 
+                </div> 
+              </form>    
+            </div>`
+  $('.modal-content').html(modal)
 }
